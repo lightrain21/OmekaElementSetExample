b711ff5e4fd837feef9d47fad710918b8b255bd0
Jeremy Boggs <jeremy@clioweb.org>
2011-03-10 19:54:00 -0500

    Changes copyright and author to Jeremy Boggs.

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version $Id$
- * @copyright Center for History and New Media, 2011
+ * @copyright Jeremy Boggs, 2011
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package OmekaElementSetExample
  */
